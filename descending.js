b9376c15f2cf3c75295954aa8962a50e7cb9cde9
Pavana P <97238636+p1a1@users.noreply.github.com>
2023-10-11 11:25:02 +0530

    Delete descending.js

--- descending.js
+++
@@ -1,8 +0,0 @@
-function arrSort(arr) { 
-	arr.sort((a,b)=>a-b); 
-	arr.reverse(); 
-	return arr; 
-} 
-var input = prompt("Please enter comma-separated values for the array:");
-var userInputArray = input.split(',');
-console.log(arrSort(userInputArray));
